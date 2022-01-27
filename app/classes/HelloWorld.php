3241c717a38cc8d6477f5f65b15929134e25e2d9
shaown <shaownakter.1995@gmail.com>
2022-01-27 13:11:46 +0600

    day_9_php

---
+++ app/classes/HelloWorld.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\classes;
+
+
+class HelloWorld
+{
+    public $name = 'BITM';
+    public function index()
+
+    {
+        // variable
+        // operator
+        // statement
+        // Array
+       $first_name = 'Hasib';
+       $last_name = 'Kabir';
+       echo $first_name.$last_name;
+
+
+    }
+}
